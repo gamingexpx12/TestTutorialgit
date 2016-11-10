897c78389b82aa2873c6e199420a8c56a6f2e90a
gamingexpx12 <gamingexpx12@gmail.com>
2016-11-10 21:34:02 +0100

    aaa

---
+++ maths.py
@@ -0,0 +1,9 @@
+def sqr(x):
+    """Return the square root of a number 'x'
+
+    Examples
+    --------
+    >>> sqr(5)
+    25
+    """
+    return x ** 2
---
+++ test_maths.py
@@ -0,0 +1,5 @@
+from maths import sqr
+
+def test_square_root():
+    x = 4
+    assert sqr(x) == 16
